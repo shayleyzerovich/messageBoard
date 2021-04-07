cbf65ca7c86024fd9abefb7857e491358fd0dc67
shayleyzerovich <58327880+shayleyzerovich@users.noreply.github.com>
2021-04-07 12:11:39 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -4,19 +4,65 @@ using namespace std;
 
 namespace ariel
 {
-    // Empty Func Just for compile and Test
+   
     void Board::post(unsigned int row, unsigned int column, Direction direction, string const & data )
     {
+        unsigned int length = 0;
+        // resize board
+        this->minRow = min(this->minRow, row);
+        this->maxRow = max(this->maxRow, row);
+        this->minCol = min(this->minCol, column);
+        this->maxCol = max(this->maxCol, column);
         
+        for(char letter: message)
+        {
+            this->board[row][column].entry = letter;
+            if(direction == Direction::Horizontal){
+                column++;
+            }
+            else // vertical
+            {
+                row++; 
+            }
+            length++;
+        }
+        if(direction == Direction::Horizontal){
+            // update board size
+                maxCol+=length;
+            }
+            else
+            {
+                maxRow+=length;
+            }
     }
-    // Empty Func Just for compile and Test
+       
     string Board::read(unsigned int row, unsigned int column, Direction direction, unsigned int length )
     {
-        return " ";
+        string message;
+        for(unsigned int i=0; i<length; i++){
+            message += this->board[row][column].entry;
+            if(direction == Direction::Horizontal){
+                column++;
+            }
+            else  // if vertical
+            {
+                row++; 
+            }        
+        }
+        return message;
     }
-    // Empty Func Just for compile and Test
+
+    }
+   
     void Board::show()
     {
-        
+      for (unsigned int row = this->minRow; row <= this->maxRow; row++) 
+        {
+            for (unsigned int col = this->minCol; col <= this->maxCol; col++) 
+            {
+                cout << this->board[row][col].entry;
+            }
+            cout << endl;
+        }  
     }
 }
