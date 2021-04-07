82e4a316beb5c18ac5f105545c06ff4279e7cf8d
shayleyzerovich <58327880+shayleyzerovich@users.noreply.github.com>
2021-04-07 12:36:28 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -12,24 +12,25 @@ using ariel::Direction;
 #include <iostream>
 #include <stdexcept>
 using namespace std;
+using namespace ariel;
 
 // Vertical - Up Down
 // Horizontal - Left Right
 
 int main() {
-	ariel::Board board;
-	board.post(/*row=*/100, /*column=*/200, Direction::Horizontal, "abcd");
-	cout << board.read(/*row=*/99, /*column=*/201, Direction::Vertical, /*length=*/3) << endl;  
-		// prints "_b_" (starts at row 99 which is empty; then at row 100 there is "b"; then row 101 is empty again).
-	board.post(/*row=*/99, /*column=*/202, Direction::Vertical, "xyz");
-	cout << board.read(/*row=*/100, /*column=*/200, Direction::Horizontal, /*length=*/6) << endl;  
-		// prints "abyd__" (First letters are ab; then y from the "xyz"; then d; then two empty cells).
 
-	board.show(); // shows the board in a reasonable way. For example:
-		//    98:  _________
-		//    99:  ____x____
-		//    100: __abyd___
-		//    101: ____z____
-		//    102: _________
+    Board *board = new Board;
+
+    board->post(0, 0, Direction::Horizontal, "Make your dreams come true");
+    board->post(1, 0, Direction::Horizontal, "Don't worry, be happy");
+    board->post(2, 0, Direction::Horizontal, "Don't let a moment go by");
+    board->post(3, 0, Direction::Horizontal, "Love the life you live and live the life you love");
+    board->post(4, 0, Direction::Horizontal, "Smile,cause it's free");
+    board->post(5, 0, Direction::Horizontal, "Nothing can stop me");
+    board->post(6, 0, Direction::Horizontal, "The sky is the limit");
+    board->post(100,50,Direction::Horizontal,"life is wonderfull just belive and live as you want to live");
+
+    board->show();
+
 }
 
